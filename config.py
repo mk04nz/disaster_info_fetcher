9139bacab9555488018b65b25586780bb07e7315
mk04nz <p.mkmk12@gmail.com>
2025-12-11 23:03:29 +0900

    Changed api url

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-#URL = "https://api.p2pquake.net/v2/history"
-URL = "https://api-v2-sandbox.p2pquake.net/v2/history"
+URL = "https://api.p2pquake.net/v2/history"
+#URL = "https://api-v2-sandbox.p2pquake.net/v2/history"
